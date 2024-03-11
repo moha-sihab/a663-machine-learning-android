ad2cbb62deb9a287fffb8853c062cf5250eff023
moha-sihab <moha.sihab@gmail.com>
2024-03-11 09:22:59 +0700

    final image classification

--- android-for-ml/starter-project-online-image-classification/app/build.gradle.kts
+++ android-for-ml/starter-project-online-image-classification/app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -49,4 +49,10 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0") //untuk lifecycleScope
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
 }
