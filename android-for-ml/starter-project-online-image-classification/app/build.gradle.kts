8015902bd82a58da7eb30bdba0d6011c07cb3689
moha-sihab <moha.sihab@gmail.com>
2024-03-16 13:17:07 +0700

    final image classification - tensor flow lite

--- android-for-ml/starter-project-online-image-classification/app/build.gradle.kts
+++ android-for-ml/starter-project-online-image-classification/app/build.gradle.kts
@@ -32,10 +32,12 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
 dependencies {
+
     val cameraxVersion = "1.3.0"
 
     implementation("androidx.core:core-ktx:1.12.0")
@@ -49,4 +51,8 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 }
